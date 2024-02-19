7a25eba747d3d7b57df6096a94fe45e6fe94d961
Jean Piere Bellota Mendoza <jeanpiere.bellota@gmail.com>
2024-02-19 20:46:00 +1100

    API added

--- server.js
+++ server.js
@@ -16,11 +16,143 @@ const db = mysql.createConnection(
     // MySQL username,
     user: 'root',
     // TODO: Add MySQL password here
-    password: '',
-    database: 'movies_db'
+    password: '12345',
+    database: 'company_db'
   },
   console.log(`Connected to the movies_db database.`)
 );
+
+// Create a department
+app.post('/api/department', ({ body }, res) => {
+    const sql = `INSERT INTO department (name)
+      VALUES (?)`;
+    const params = [body.name];
+    
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
+    });
+  });
+
+  // Read all departments
+app.get('/api/departments', (req, res) => {
+    const sql = `SELECT a.id, a.name
+                 FROM department a
+                 ORDER BY a.name;`;
+    
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+         return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+  // Create a role
+app.post('/api/role', ({ body }, res) => {
+  const sql = `INSERT INTO role (title, salary, department_id)
+    VALUES (?, ?, ?)`;
+  const params = [body.title, body.salary, body.department_id];
+  
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
+});
+
+// Read all roles
+app.get('/api/roles', (req, res) => {
+  const sql = `SELECT a.id, a.title, b.name, a.salary
+               FROM role a
+               JOIN department b ON b.id = a.department_id;`;
+  
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+       return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// Create a employee
+app.post('/api/employee', ({ body }, res) => {
+  const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    VALUES (?, ?, ?, ?)`;
+  const params = [body.first_name, body.last_name, body.role_id, body.manager_id];
+  
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
+});
+
+// Read all employees
+app.get('/api/employees', (req, res) => {
+  const sql = `SELECT a.id, a.first_name, a.last_name, b.title, c.name, CONCAT(d.first_name, ' ', d.last_name) as manager
+               FROM employee a
+               JOIN role b ON b.id = a.role_id
+               JOIN department c ON c.id = b.department_id
+               LEFT JOIN employee d ON d.id = a.manager_id;`;
+  
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+       return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// Update employee rol
+app.put('/api/employee/:id', (req, res) => {
+  const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+  const params = [req.body.role_id, req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Employee not found'
+      });
+    } else {
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: result.affectedRows
+      });
+    }
+  });
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
